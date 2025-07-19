255099a849e1f244ba2c1da2a626404438e6dc37
NerdyOgre254 <ben.paul.birchall@gmail.com>
2025-07-19 17:14:31 +1000

    external function tutorial

--- reactlearn/src/App.jsx
+++ reactlearn/src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-
+/*
 const welcome = {
   greeting: "Hey",
   title: "Ben!",
 };
+*/
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   // you can do calculations and stuff here
   const title = "React!";
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>Hey there, {getTitle("Ben!")}</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
