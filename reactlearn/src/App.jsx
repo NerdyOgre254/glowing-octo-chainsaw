2aba0d4832d373ad462468493d292744de922aaf
NerdyOgre254 <ben.paul.birchall@gmail.com>
2025-07-19 17:11:32 +1000

    object reference tutorial

--- reactlearn/src/App.jsx
+++ reactlearn/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 
+const welcome = {
+  greeting: "Hey",
+  title: "Ben!",
+};
+
 function App() {
   // you can do calculations and stuff here
   const title = "React!";
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
