22d79448cb36a59844e2adad6beb308ab6d0cb84
NerdyOgre254 <ben.paul.birchall@gmail.com>
2025-07-19 16:58:39 +1000

    initial commit

---
+++ reactlearn/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  // you can do calculations and stuff here
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
