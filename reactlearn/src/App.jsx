8bf383e125f832397e356b19c33c60fb16f95aec
NerdyOgre254 <ben.paul.birchall@gmail.com>
2025-07-19 17:33:23 +1000

    Array map and list tutorial

--- reactlearn/src/App.jsx
+++ reactlearn/src/App.jsx
@@ -7,6 +7,25 @@ const welcome = {
 };
 */
 
+const list = [
+  {
+    title: "React",
+    url: "https://react.dev",
+    author: "Ghazghkull",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Kaptain Badrukk",
+    num_comments: 5,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function getTitle(title) {
   return title;
 }
@@ -16,9 +35,27 @@ function App() {
   const title = "React!";
   return (
     <div>
-      <h1>Hey there, {getTitle("Ben!")}</h1>
+      <h1>My Orky Waaagh Stories</h1>
+
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title} </a>
+              </span>
+              <span>{item.author} </span>
+              <span>{item.num_comments} </span>
+              <span>{item.points} </span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
