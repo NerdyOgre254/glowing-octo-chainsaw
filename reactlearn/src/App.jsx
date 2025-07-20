953ff788eb7509bf07254228a0baa7b7fb3db23a
NerdyOgre254 <ben.paul.birchall@gmail.com>
2025-07-20 20:57:07 +1000

    extract functions tutorial

--- reactlearn/src/App.jsx
+++ reactlearn/src/App.jsx
@@ -30,6 +30,25 @@ function getTitle(title) {
   return title;
 }
 
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title} </a>
+            </span>
+            <span>{item.author} </span>
+            <span>{item.num_comments} </span>
+            <span>{item.points} </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
 function App() {
   // you can do calculations and stuff here
   const title = "React!";
@@ -42,20 +61,7 @@ function App() {
 
       <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title} </a>
-              </span>
-              <span>{item.author} </span>
-              <span>{item.num_comments} </span>
-              <span>{item.points} </span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
   );
 }
