6b11236924e69dbbcea4d6f9d2177086b2fc1f93
NerdyOgre254 <ben.paul.birchall@gmail.com>
2025-07-22 13:56:05 +1000

    format span

--- reactlearn/src/App.jsx
+++ reactlearn/src/App.jsx
@@ -38,11 +38,11 @@ const List = (props) => (
 const Item = (props) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={props.item.url}>{props.item.title} </a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{props.item.author} </span>
+    <span>{props.item.num_comments} </span>
+    <span>{props.item.points} </span>
   </li>
 );
 
