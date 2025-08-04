7d97aecd2698eaeb0e0dec7c89ac1373cbf35431
NerdyOgre254 <ben.paul.birchall@gmail.com>
2025-08-04 20:10:10 +1000

    Unknown error, replaced all code with tutorial code to fix

--- reactlearn/src/App.jsx
+++ reactlearn/src/App.jsx
@@ -1,41 +1,57 @@
 import * as React from "react";
-import { useState } from "react";
-import "./App.css";
-/*
-const welcome = {
-  greeting: "Hey",
-  title: "Ben!",
-};
-*/
 
-const getTitle = (title) => {
-  return title;
-};
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const Search = (props) => {
-  console.log("Search Renders");
   const [searchTerm, setSearchTerm] = React.useState("");
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
 
-    props.onSearch(event);
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
+      <h1>My Hacker Stories</h1>
 
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
+      <Search onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={searchedStories} />
     </div>
   );
 };
 
+const Search = (props) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch} />
+  </div>
+);
+
 const List = (props) => (
   <ul>
-    {console.log("List Renders")}
     {props.list.map((item) => (
       <Item key={item.objectID} item={item} />
     ))}
@@ -44,53 +60,13 @@ const List = (props) => (
 
 const Item = (props) => (
   <li>
-    {console.log("Item Renders")}
     <span>
-      <a href={props.item.url}>{props.item.title} </a>
+      <a href={props.item.url}>{props.item.title}</a>
     </span>
-    <span>{props.item.author} </span>
-    <span>{props.item.num_comments} </span>
-    <span>{props.item.points} </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
   </li>
 );
-
-const App = () => {
-  // you can do calculations and stuff here
-  const title = "React!";
-  console.log("App Renders");
-  const stories = [
-    {
-      title: "React",
-      url: "https://react.dev",
-      author: "Ghazghkull",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org",
-      author: "Kaptain Badrukk",
-      num_comments: 5,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  //A
-  const handleSearch = (event) => {
-    console.log(event.target.value);
-  };
-
-  return (
-    <div>
-      <h1>My Orky Waaagh Stories</h1>
-      <Search onSearch={handleSearch} />
-      <hr />
-
-      <List list={stories} />
-    </div>
-  );
-};
 
 export default App;
